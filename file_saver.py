0cae27a6f6e7e594bfef742fd3d45f0c262ffbf7
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2023-10-20 21:58:08 +0200

    save_data_to_excel function modification.

--- file_saver.py
+++ file_saver.py
@@ -2,5 +2,7 @@ import pandas as pd
 
 
 def save_data_to_excel(data, excel_path):
-    df = pd.DataFrame(data[1:], columns=data[0])
+    expected_columns = len(data[0])
+    filtered_data = [row for row in data if len(row) == expected_columns]
+    df = pd.DataFrame(filtered_data[1:], columns=filtered_data[0])
     df.to_excel(excel_path, index=False)
