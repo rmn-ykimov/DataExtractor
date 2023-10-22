cdf5b78ed29c72be6c150d0474ea338703eaf3c5
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2023-10-22 20:49:18 +0200

    Modifications for table_extractor module.

--- file_saver.py
+++ file_saver.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 
 def save_data_to_excel(data, excel_path):
+    # Use the number of columns in the first row as the expected number
     expected_columns = len(data[0])
+
+    # Filter out rows with a different number of columns
     filtered_data = [row for row in data if len(row) == expected_columns]
+
+    # Create DataFrame and save to Excel
     df = pd.DataFrame(filtered_data[1:], columns=filtered_data[0])
     df.to_excel(excel_path, index=False)
--- table_extractor.py
+++ table_extractor.py
@@ -2,20 +2,27 @@ from docx import Document
 
 
 def process_text(text):
-    text = text.replace('-\n', '').replace('\n', ' ')
-    return text
+    return text.replace('-\n', '').replace('\n', ' ')
+
+
+def is_section_year_row(row):
+    return any("г." in cell_content for cell_content in row)
 
 
 def extract_table_data(docx_path):
     document = Document(docx_path)
     table_data = []
+    current_section_year = None
 
     for table in document.tables:
         for row in table.rows:
-            row_data = []
-            for cell in row.cells:
-                processed_text = process_text(cell.text.strip())
-                row_data.append(processed_text)
+            row_data = [process_text(cell.text.strip()) for cell in row.cells]
+
+            if is_section_year_row(row_data):
+                current_section_year = next((cell_content for cell_content in row_data if "г." in cell_content), None)
+                continue
+
+            row_data.append(current_section_year)
             table_data.append(row_data)
 
     return table_data
