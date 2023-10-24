44255245388e4558378175b76c37cccfdd597763
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2023-10-24 21:12:24 +0200

    Changing in logic of additional row naming.

--- table_extractor.py
+++ table_extractor.py
@@ -13,16 +13,25 @@ def extract_table_data(docx_path):
     document = Document(docx_path)
     table_data = []
     current_section_year = None
+    headers_added = False
 
     for table in document.tables:
         for row in table.rows:
             row_data = [process_text(cell.text.strip()) for cell in row.cells]
 
             if is_section_year_row(row_data):
-                current_section_year = next((cell_content for cell_content in row_data if "г." in cell_content), None)
+                current_section_year = next(
+                    (cell_content for cell_content in row_data if
+                     "г." in cell_content), None)
                 continue
 
-            row_data.append(current_section_year)
+            if not headers_added:
+                row_data.append("Год (Раздел)")
+                headers_added = True
+
+            else:
+                row_data.append(current_section_year)
+
             table_data.append(row_data)
 
     return table_data
